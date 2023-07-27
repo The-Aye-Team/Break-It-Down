67983ea29ecb9bb10091796d17f90d9cde239b7e
Andy Ficek <aficek@precisionplanting.com>
2023-07-27 16:02:56 -0500

    added delete button functionality as well as improved edit button functionality

--- assets/js/completescript.js
+++ assets/js/completescript.js
@@ -1,19 +1,50 @@
 // SETTING READ ONLY TRUE AND FALSE
 function finishEdit() {
-    if (document.querySelector(".subTaskText").readOnly == true) {
-        document.querySelector('.subTaskText').readOnly = false;
-        document.querySelector(`.subTaskText`).focus();
-    } else {
-        document.querySelector('.subTaskText').readOnly = true
+    for (i=0; i < subTaskTextArr; i++) {
+        if (document.querySelector(".subTaskText").readOnly == true) {
+            document.querySelector('.subTaskText').readOnly = false;
+            document.querySelector(`.subTaskText`).focus();
+        } else {
+            document.querySelector('.subTaskText').readOnly = true
+        }
     }
 };
+function deleteSubTask(e) {
+    e.currentTarget.parentNode.remove();
+}
 // CLICK EVENT ON EDIT BUTTON 
-document.querySelector(`.editBtn`).addEventListener('click', finishEdit);
-// LISTENING FOR ENTER KEY 
-document.querySelector(".subTaskText").addEventListener("keypress", (e) => {
-    if (e.key === `Enter`) {
-      finishEdit();
-    }
-  });
+let subTaskArr = document.querySelectorAll(`.subTaskWrapper`);
 
-  
+
+
+for (i = 0; i < subTaskArr.length; i++) {
+    function finishEdit() {
+        if (subTaskText.readOnly == true) {
+            subTaskText.readOnly = false;
+            subTaskText.focus();
+        } else {
+            subTaskText.readOnly = true
+        }
+    };
+
+    let editBtn = subTaskArr[i].querySelector(`.editBtn`);
+    let subTaskText = subTaskArr[i].querySelector(`.subTaskText`);
+    let deleteBtn = subTaskArr[i].querySelector(`.deleteBtn`);
+
+    deleteBtn.addEventListener(`click`, (e) => {
+        deleteSubTask(e);
+    });
+
+    editBtn.addEventListener(`click`, () => {
+        finishEdit();
+    });
+    
+    // LISTENING FOR ENTER KEY 
+    subTaskText.addEventListener("keypress", (e) => {
+            if (e.key === `Enter`) {
+              finishEdit();
+            }
+          });
+};
+
+
